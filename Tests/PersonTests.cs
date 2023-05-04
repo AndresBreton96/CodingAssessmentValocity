477f549ae772da8ef6a5f0734a3c6841cf190b72
Andres Breton <andres_breton96@outlook.com>
2023-05-04 21:52:16 +0000

    Refactoring 1.1
    - Created the tests for the second method of the CodeToRefactor (GetBobs).

--- Tests/PersonTests.cs
+++ Tests/PersonTests.cs
@@ -56,8 +56,8 @@ namespace Tests
             var random = new Random();
             int numberOfPeople = random.Next(0, 5);
             var agesOutOfRange = false;
-            var dateFor18 = DateTime.UtcNow.Subtract(new TimeSpan(18 * 365, 0, 0, 0));
-            var dateFor85 = DateTime.UtcNow.Subtract(new TimeSpan(85 * 365, 0, 0, 0));
+            var dobFor18 = DateTime.UtcNow.Subtract(new TimeSpan(18 * 365, 0, 0, 0));
+            var dobFor85 = DateTime.UtcNow.Subtract(new TimeSpan(85 * 365, 0, 0, 0));
             
             // Act
             var people = _birthingUnit.GetPeople(numberOfPeople);
@@ -66,10 +66,59 @@ namespace Tests
             // The list returned should contain only people with ages within 18 to 85.
             foreach (var person in people)
             {
-                agesOutOfRange = (person.DOB > dateFor18 || person.DOB < dateFor85);
+                agesOutOfRange = (person.DOB >= dobFor18 || person.DOB <= dobFor85);
             }
 
             Assert.True(!agesOutOfRange);
+        }
+
+        [Fact]
+        public void GetBobs_ShouldReturnOnlyBobsOlderThan30()
+        {
+            // Arrange
+            // Set the number of people to create and set variables.
+            var random = new Random();
+            int numberOfPeople = random.Next(0, 5);
+            var olderThan30 = true;
+            var differentNameOrAgeExists = false;
+            var dobFor30 = DateTime.UtcNow.Subtract(new TimeSpan(30 * 365, 0, 0, 0));
+            
+            // Act
+            var people = _birthingUnit.GetPeople(numberOfPeople);
+            var bobs = _birthingUnit.GetBobs(olderThan30);
+
+            // Assert
+            // The list returned should contain only Bob.
+            foreach (var person in people)
+            {
+                differentNameOrAgeExists = (person.Name != "Bob" || person.DOB <= dobFor30);
+            }
+
+            Assert.True(!differentNameOrAgeExists);
+        }
+
+        [Fact]
+        public void GetBobs_ShouldReturnOnlyBobsAnyAge()
+        {
+            // Arrange
+            // Set the number of people to create and set variables.
+            var random = new Random();
+            int numberOfPeople = random.Next(0, 5);
+            var olderThan30 = false;
+            var differentNameExists = false;
+            
+            // Act
+            var people = _birthingUnit.GetPeople(numberOfPeople);
+            var bobs = _birthingUnit.GetBobs(olderThan30);
+
+            // Assert
+            // The list returned should contain only Bob.
+            foreach (var person in people)
+            {
+                differentNameExists = person.Name != "Bob";
+            }
+
+            Assert.True(!differentNameExists);
         }
     }
 }
